1c49594253587baab2013a9041d7e6b9fa0be3ec
Noa Kim <noanonoa@gmail.com>
2020-08-04 23:57:02 -0700

    part 1 complete

--- solution.py
+++ solution.py
@@ -1,3 +1,5 @@
+import geocoder
+
 destinations = [
   "Space Needle",
   "Crater Lake",
@@ -12,3 +14,12 @@ destinations = [
   "Banff National Park",
   "Capilano Suspesion Bridge"
 ]
+
+g = geocoder.arcgis('Redlands, CA')
+
+print(g.latlng[0], g.latlng[1])
+print('🔥')
+
+for i in destinations:
+  geo = geocoder.arcgis(i)
+  print("{} is located at ({:.4f}, {:.4f})".format(i, geo.latlng[0], geo.latlng[1]))
