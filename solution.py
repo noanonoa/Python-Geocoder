076d1487ec34c527b7b434e96bb3fe468f31f7c0
Noa Kim <noanonoa@gmail.com>
2020-08-04 22:35:07 -0700

    initial commit, destinations list

---
+++ solution.py
@@ -0,0 +1,14 @@
+destinations = [
+  "Space Needle",
+  "Crater Lake",
+  "Golden Gate Bridge",
+  "Yosemite National Park",
+  "Las Vegas, Nevada",
+  "Grand Canyon National Park",
+  "Aspen, Colorado",
+  "Mount Rushmore",
+  "Yellowstone National Park",
+  "Sandpoint, Idaho",
+  "Banff National Park",
+  "Capilano Suspesion Bridge"
+]
